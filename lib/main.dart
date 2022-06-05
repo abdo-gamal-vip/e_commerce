78658b4681f4525fbcdd699f69c1210dc1679401
abdo-gamal-vip <abdo.gamal.vip@gmail.com>
2022-06-05 07:50:33 +0200

    wow

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:e_commerce/helper/conests.dart';
 import 'package:e_commerce/theme/theme.dart';
 import 'package:e_commerce/views/auth_view/sign_in_view.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'login.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     title: "E-commerce",
