1e5cdcc64b167bce48b68a6b1f2528eef6d054ae
abdo-gamal-vip <abdo.gamal.vip@gmail.com>
2022-06-07 21:14:17 +0200

    ok

--- lib/views/home/home_vIew.dart
+++ lib/views/home/home_vIew.dart
@@ -1,6 +1,8 @@
+import 'package:e_commerce/view_models/home_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
