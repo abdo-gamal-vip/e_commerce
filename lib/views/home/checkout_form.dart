09caf385eefd7eb6b483d85d46fa84d5e3f0a45c
abdo-gamal-vip <abdo.gamal.vip@gmail.com>
2022-07-17 04:15:40 +0200

    modified:   lib/views/home/checkout_form.dart

--- lib/views/home/checkout_form.dart
+++ lib/views/home/checkout_form.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/widgets/small_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -29,41 +30,117 @@ class Checkform extends StatelessWidget {
           )),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          SizedBox(
-            height: 20,
-          ),
-          Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-            Image.asset("assets/checkbox.png"),
-            Text(
-              "Billing address is the same as delivery address",
-              style: TextStyle(fontSize: 14),
-            )
-          ]),
-          SizedBox(
-            height: 20,
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "street 1 ",
-                style: TextStyle(color: Colors.grey),
+        child: SingleChildScrollView(
+          child: Expanded(
+            flex: 5,
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              SizedBox(
+                height: 20,
               ),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: "21, Alex Davidson Avenue",
-                  hintStyle: TextStyle(color: Colors.black),
-                  focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black)),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+                Image.asset("assets/checkbox.png"),
+                Text(
+                  "Billing address is the same as delivery address",
+                  style: TextStyle(fontSize: 14),
+                )
+              ]),
+              SizedBox(
+                height: 20,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "street 1 ",
+                    style: TextStyle(color: Colors.grey),
                   ),
-                ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "21, Alex Davidson Avenue",
+                      hintStyle: TextStyle(color: Colors.black),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black)),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "street 2 ",
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Opposite Omegatron, Vicent Quarters",
+                      hintStyle: TextStyle(color: Colors.black),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black)),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "City ",
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Cairo .. alex .. elmanya",
+                      hintStyle: TextStyle(color: Colors.black),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black)),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text(
+                        "State ",
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      Text(
+                        "Countery ",
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Container(
+                          width: 100,
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              hintText: "Lagos State",
+                              hintStyle: TextStyle(color: Colors.black),
+                              focusedBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.black)),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: 100,
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              hintText: "Nigeria",
+                              hintStyle: TextStyle(color: Colors.black),
+                              focusedBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.black)),
+                            ),
+                          ),
+                        ),
+                      ]),
+                  Row(children: [defSmallButton(txt: "Back", ontap: () {})]),
+                ],
               ),
-            ],
+            ]),
           ),
-        ]),
+        ),
       ),
     ));
   }
